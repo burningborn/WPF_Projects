619a4746bcd01c7a23dbf6ac1f15bbca5c9bb0a5
burningborn <evgenyborn@gmail.com>
2022-02-15 14:08:50 +0400

    Добавьте файлы проекта.

---
+++ SSP_Game/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SSP_Game
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SSP_Game You = new SSP_Game();
+        private SSP_Game Computer = new SSP_Game();
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myMove_Click(object sender, RoutedEventArgs e)
+        {
+            youBox.Text = "";
+            computerBox.Text = "";
+            outText.Text = "";
+        }
+        public void resultFunc(SSP_Game one, SSP_Game two)
+        {
+            int key = SSP_Game.Fight(one, two);
+            switch (key)
+            {
+                case 1:
+                    outText.Text = "Вы выиграли";
+                    break;
+                case -1:
+                    outText.Text = "Выиграл компьтер";
+                    break;
+                case 0:
+                    outText.Text = "НИЧЬЯ";
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                Computer.Bones();
+            computerBox.Text = Computer.status;
+            do
+            {
+            You.Bones();
+            }
+            while (You.status == Computer.status);
+            youBox.Text = You.status;
+
+            resultFunc(You, Computer);
+        }       
+    }
+}
---
+++ SSP_Game/SSP_Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSP_Game
+{
+    public class SSP_Game
+    {
+        public string[] Game = new string[] { "Stone", "Scissors", "Paper" };      
+        public string status = null;
+
+
+        public void Bones()
+        {
+            Random rnd = new Random();
+            this.status = Game[rnd.Next(0, 3)];
+        }
+
+        static public int Fight(SSP_Game one, SSP_Game two)
+        {
+            int key = 3;
+            if (one.status == "Stone" && two.status == "Stone")
+                key = 0;
+            if (one.status == "Stone" && two.status == "Scissors")
+                key = 1;
+            if (one.status == "Stone" && two.status == "Paper")
+                key = -1;
+
+            if (one.status == "Scissors" && two.status == "Scissors")
+                key = 0;
+            if (one.status == "Scissors" && two.status == "Paper")
+                key = 1;
+            if (one.status == "Scissors" && two.status == "Stone")
+                key = -1;
+
+            if (one.status == "Paper" && two.status == "Paper")
+                key = 0;
+            if (one.status == "Paper" && two.status == "Stone")
+                key = 1;
+            if (one.status == "Paper" && two.status == "Scissors")
+                key = -1;
+
+            return key;
+        }
+    }
+}
